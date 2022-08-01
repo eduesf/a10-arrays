81e7694c3754f239b09f4314266ce145e0f54377
Fernandes Eduardo <108585424+eduesf@users.noreply.github.com>
2022-08-01 14:16:48 -0300

    Revert "Resolução dos 3 Exercícios de Array - Eduardo Fernandes - Barbosa B"

--- exercicio1/script.js
+++
@@ -1,9 +0,0 @@
-// Resolução do Exercício 1
-
-const arrayNum = [13,44,9,8,38,19,5,2]
-const arrayString = ["Coxinha", "Quibe", "Esfiha", "Bolinho de Carne"]
-const arrayMisto = [13,44,9,"Coxinha", "Quibe", "Esfiha", true, true, false]
-
-console.log (arrayNum);                  // Imprimindo as arrays
-console.log (arrayString);
-console.log (arrayMisto);
